bd7566e24a7bb7cc5ca7605a94df305f53a68a76
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-03-01 11:04:18 +0100

    Add env template and load allowed channels from env

--- src/variables.py
+++ src/variables.py
@@ -1,12 +1,13 @@
 
 import os
+import json
 
-# variables 
+# variables
 prefix = os.environ.get("PREFIX")
 embedColor = os.environ.get("EMBED_COLOR")
 icon = os.environ.get("ICON")
 
-allowedChannels = [815203001396494376, 917838518229815346, 934174018800590848, os.environ.get("BOT_INTERACTION_CHANNEL_ID")]
+allowedChannels = json.loads(os.environ['BOT_INTERACTION_CHANNEL_ID'])
 
 wallet_ERC20 = "soon"
 wallet_akash = "soon"
